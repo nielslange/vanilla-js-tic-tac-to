8ce8b8075af4d3c08930d1e7d341449bb2171c85
Niels Lange <info@nielslange.de>
2021-04-09 18:20:04 +0700

    👌 IMPROVE: Finalize first working version

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
-let turn = 'x';
+(function () {
 
-const combinations = [
-    [ 1,1,1,0,0,0,0,0,0 ],
-    [ 0,0,0,1,1,1,0,0,0 ],
-    [ 0,0,0,0,0,0,1,1,1 ],
-    [ 1,0,0,1,0,0,1,0,0 ],
-    [ 0,1,0,0,1,0,0,1,0 ],
-    [ 0,0,1,0,0,1,0,0,1 ],
-    [ 1,0,0,0,1,0,0,0,1 ],
-    [ 0,0,1,0,1,0,1,0,0 ],
-];
+    let currentPlayer = 'x';
+    let playerOne = [];
+    let playerTwo = [];
 
-function toggleTurn() {
-    turn = turn == 'x' ? 'o' : 'x';
-}
+    const combinations = [
+        [ 1,2,3 ], // → 1st row left to right
+        [ 4,5,6 ], // → 2nd row left to right
+        [ 7,8,9 ], // → 3rd row left to right
+        [ 1,4,7 ], // ↓ 1st column top to bottom
+        [ 2,5,8 ], // ↓ 2nd column top to bottom
+        [ 3,6,9 ], // ↓ 3rd column top to bottom
+        [ 1,5,9 ], // ↘ top-left to bottom-right
+        [ 3,5,7 ], // ↙ top-right to bottom-left
+    ];
 
-function placeMark( field ) {
-    field.innerText = turn;
-}
+    function toggleTurn() {
+        if ( 'x' == currentPlayer ) {
+            currentPlayer = 'o';    
+        } else {
+            currentPlayer = 'x';
+        }
+    }
 
-document.addEventListener( 'click', event => {
-    if ( ! event.target.classList.contains( 'field' ) ) return;
-    
-    if ( event.target.innerText ) return;
+    function placeMarker( field ) {
+        const index = field.dataset.field;
+        'x' == currentPlayer ? playerOne.push(+index) : playerTwo.push(+index);
+        field.innerText = currentPlayer;
+        field.setAttribute( 'class', 'disabled' );
 
-    placeMark( event.target );
-    
-    toggleTurn();
+        checkState();
+    }
 
-} );
+    function countMarker() {
+        return playerOne.length + playerTwo.length;
+    }
+
+    async function checkState() {
+        const array = 'x' == currentPlayer ? playerOne : playerTwo;
+        let hasWinner = false;
+
+        combinations.forEach( combination => {
+            if ( combination.every( val => array.includes( val ) ) ) {
+                hasWinner = true;
+                showWinnerMessage();
+            }
+        } );
+
+        if ( ! hasWinner && 9 === countMarker() ) {
+            showDrawMessage();
+        }
+
+        await sleep(100);
+        toggleTurn();
+    }
+
+    function sleep( ms ) {
+        return new Promise(resolve => setTimeout( resolve, ms ) );
+    }
+
+    async function showWinnerMessage() {
+        await sleep(100);
+        if ( confirm(`${currentPlayer} is the winner! Wanna play again?`) ) {
+            window.location = window.location;
+        }
+    }
+
+    async function showDrawMessage() {
+        await sleep(100);
+        if ( confirm('No more moves possible! Wanna play again?') ) {
+            window.location = window.location;
+        }
+    }
+
+    document.addEventListener( 'click', event => {
+        if ( ! event.target.dataset.field ) return;
+        
+        if ( event.target.innerText ) return;
+
+        placeMarker( event.target );
+    } );
+
+})();
